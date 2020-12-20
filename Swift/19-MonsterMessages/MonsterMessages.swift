3209391f42aa9c71b298e0ab2c00f32cfd4d5b9e
Tom Aylesworth <tom.aylesworth@appian.com>
2020-12-19 19:06:01 -0500

    Day 19: Monster Messages part 1
    It works. It's ugly and slow but it works. Not a fan.

--- Swift/19-MonsterMessages/MonsterMessages.swift
+++ Swift/19-MonsterMessages/MonsterMessages.swift
@@ -59,6 +59,13 @@ enum PartialRule: CustomStringConvertible {
         return false
     }
 
+    var ruleIndex: Int? {
+        if case let .rule(index) = self {
+            return index
+        }
+        return nil
+    }
+
     var isLiteral: Bool {
         if case .literal = self {
             return true
@@ -102,12 +109,10 @@ func parse(partialRulesString: String) -> [[PartialRule]] {
     }
 }
 
-func isSingleLiteral(_ partialRulesList: [[PartialRule]]) -> Bool {
-    return partialRulesList.count == 1 && partialRulesList[0].count == 1 && partialRulesList[0][0].isLiteral
-}
-
-func isMultipleLiteral(_ partialRulesList: [[PartialRule]]) -> Bool {
-    return partialRulesList.allSatisfy { $0.allSatisfy { $0.isLiteral } }
+func print(_ rules: Rules) {
+    for index in rules.keys.sorted() {
+        print("\(index): \(rules[index]!)")
+    }
 }
 
 func joinLiterals(partialRules: [PartialRule]) -> PartialRule? {
@@ -128,91 +133,37 @@ func joinLiterals(partialRulesList: [[PartialRule]]) -> [[PartialRule]] {
     }
 }
 
-func update(_ rules: Rules, replacing ruleIndexToReplace: Int, with newPartialRule: PartialRule) -> Rules {
-    let updatedRules = rules.map { (ruleIndex, partialRulesList) -> (Int, [[PartialRule]]) in
-        let updatedPartialRulesList = partialRulesList.map { (partialRules) -> [PartialRule] in
-            partialRules.map { (partialRule) -> PartialRule in
-                guard case let .rule(index) = partialRule else { return partialRule }
-                return index == ruleIndexToReplace ? newPartialRule : partialRule
-            }
-        }
-        let flattenedRulesList = joinLiterals(partialRulesList: updatedPartialRulesList)
-        return (ruleIndex, flattenedRulesList)
-    }
-    return Dictionary(uniqueKeysWithValues: updatedRules)
-}
-
-func update(_ rules: Rules, replacing ruleIndexToReplace: Int, with newPartialRules: [[PartialRule]]) -> Rules {
-    let updatedRules = rules.map { (ruleIndex, rulesList) -> (Int, [[PartialRule]]) in
-        var newRulesList = [[PartialRule]]()
-        for ruleIndices in rulesList {
-            for (index, partialRule) in ruleIndices.enumerated() {
-                if case let .rule(ruleIndex) = partialRule, ruleIndex == ruleIndexToReplace {
-                    let prefix = ruleIndices.prefix(upTo: index)
-                    let suffix = ruleIndices.suffix(from: index+1)
-                    let permutations = newPartialRules.map {
-                        Array(prefix + $0 + suffix)
-                    }
-                    newRulesList.append(contentsOf: permutations)
-                }
-            }
-        }
-        let flattenedRulesList = joinLiterals(partialRulesList: newRulesList)
-        let updatedRules = flattenedRulesList.count > 0 ? flattenedRulesList : rulesList
-        return (ruleIndex, updatedRules)
-    }
-    return Dictionary(uniqueKeysWithValues: updatedRules)
-}
-
-func reduce(rules: Rules) -> Rules {
-    var newRulesList = rules
-
-    var rulesToReduce = Set(rules.keys)
-    while let singleLiteralRule = rulesToReduce.first(where: { isSingleLiteral(newRulesList[$0]!) }) {
-        let literal = newRulesList[singleLiteralRule]![0][0]
-        newRulesList = update(newRulesList, replacing: singleLiteralRule, with: literal)
-        rulesToReduce.remove(singleLiteralRule)
+func permutations(of partialRules: [PartialRule], prefix: [PartialRule], from rules: Rules) -> [[PartialRule]] {
+    guard let partialRule = partialRules.first else { return [prefix] }
+    guard let ruleIndex = partialRule.ruleIndex else {
+        // Replacement is a literal so simply replace it.
+        let newPrefix = prefix + [partialRule]
+        return permutations(of: Array(partialRules.dropFirst()), prefix: newPrefix, from: rules)
     }
 
-    while let multipleLiteralRule = rulesToReduce.first(where: { isMultipleLiteral(newRulesList[$0]!) }) {
-        let literals = newRulesList[multipleLiteralRule]!
-        newRulesList = update(newRulesList, replacing: multipleLiteralRule, with: literals)
-        rulesToReduce.remove(multipleLiteralRule)
+    let replacementRules = rules[ruleIndex]!
+    if replacementRules.count == 1 {
+        // Replacement is a single list of rules so replace the single rule with the list and recurse.
+        let newPartialRules = replacementRules[0] + partialRules.dropFirst()
+        return permutations(of: Array(newPartialRules), prefix: prefix, from: rules)
     }
 
-    // while !rulesToReduce.isEmpty {
-
-    // }
-
-    if !rulesToReduce.isEmpty {
-        print("Rules remaining:")
-        for rule in rulesToReduce {
-            print("  \(rule): \(newRulesList[rule]!)")
-        }
+    var thePermutations = [[PartialRule]]()
+    for replacementPartialRules in replacementRules {
+        // Replacement is a list of subrules so create the permutations and recurse.
+        let newPartialRules = replacementPartialRules.map { $0 } + partialRules.dropFirst()
+        thePermutations.append(Array(newPartialRules))
     }
-    //assert(rulesToReduce.isEmpty)
-
-    return newRulesList
-}
-
-func print(_ rules: Rules) {
-    for index in rules.keys.sorted() {
-        print("\(index): \(rules[index]!)")
-    }
+    return Array(thePermutations.map { permutations(of: $0, prefix: prefix, from: rules) }.joined())
 }
 
 //let input = mockData()
 let input = readFile(named: "19-input")
 let (rules, messages) = parse(input)
-print(rules)
-print()
 
-let reducedRules = reduce(rules: rules)
-print(reducedRules)
-// print()
+let rule0 = permutations(of: rules[0]![0], prefix: [], from: rules)
+let rule0Joined = joinLiterals(partialRulesList: rule0)
+let rule0Strings = Set(rule0Joined.compactMap { $0.first?.literalValue } )
 
-let rule0Values = Set(reducedRules[0]!.compactMap { $0.first?.literalValue })
-print(rule0Values)
-
-let matches = messages.filter { rule0Values.contains($0) }.count
-print(matches)
+let matches = messages.filter { rule0Strings.contains($0) }.count
+print("\(matches) messages completely match rule 0")
